c069d9e53b549571487833e08837b358bed40d41
Javi H. Gil <javihgil@softspring.es>
2023-01-02 21:31:48 +0100

    Add filters mode

--- src/Paginator.php
+++ src/Paginator.php
@@ -19,18 +19,18 @@ class Paginator
      * @throws InvalidFilterValueException
      * @throws MissingFromInQueryBuilderException
      */
-    public static function queryPage(QueryBuilder $qb, int $page, int $rpp = null, array $filters = [], array $orderBy = []): PaginatedCollection
+    public static function queryPage(QueryBuilder $qb, int $page, int $rpp = null, array $filters = [], array $orderBy = [], int $filtersMode = Filters::MODE_AND): PaginatedCollection
     {
         // get total results
         $cloneQb = clone $qb;
-        Filters::apply($cloneQb, $filters);
+        Filters::apply($cloneQb, $filters, $filtersMode);
         $cloneQb->select('COUNT('.$cloneQb->getAllAliases()[0].')');
         $total = (int) $cloneQb->getQuery()->getSingleScalarResult();
 
         if ($total) {
             $qb->setFirstResult(($page - 1) * $rpp);
             $qb->setMaxResults($rpp);
-            Filters::apply($qb, $filters);
+            Filters::apply($qb, $filters, $filtersMode);
             Filters::sortBy($qb, $orderBy);
             $result = $qb->getQuery()->getResult();
         }
